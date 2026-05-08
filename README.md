6a0b36e0f4406abb211caeef8375833d9d787368
Lucas-nepomuceno <lucas.santos@sou.inteli.edu.br>
2026-05-08 11:57:22 -0300

    realiza a ponderada

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ponderada-base-m10-01
-Atividade Ponderada 1
+# Lançador de Dados
+
+&emsp; Este repositório contém uma aplicação Android em Kotlin para um lançador de dados. Ele faz parte de uma atividade avaliativa em sala de aula no Inteli. 
+
+## Raciocínio para a resolução do problema
+
+&emsp; Primeiro, forkei o repositório e clonei-o no meu Android Studio. Em seguida, resolvi os problemas lógicos do código enquanto o aplicativo buildava em meu celular. O problema lógico era que devido ao funcionamento da função ``Random.nextInt(6)``, o dado estava dando os seguintes resultados: **0**, 1, 2, 3, 4, 5. Primeiro, apenas percebi o problema de não estar indo até 6, então, só mudei a função para ``Random.nextInt(7)``. Depois de alguns testes, porém, percebi o problema do 0 e estudando a documentação: https://api.flutter.dev/flutter/dart-math/Random/nextInt.html, mudei a linha para ``Random.nextInt(6) + 1``. Perceba que essa documentação é do Flutter não do Kotlin. Se eu tivesse percebido isso, teria usado ``Random.nextInt(from = 1, until = 7)``, mas o método do Flutter também funcionou.
+
+&emsp; Em seguida, adicionei os outros dados. Para tanto, apenas alterei o ``val dados = listOf("D6")`` para ``val dados = listOf("D6", "D10", "D20", "D100")``. Além disso, adicionei linhas como ``"D10" -> Random.nextInt(10) + 1`` no valorSorteado. Essa etapa aconteceu sem problemas.
+
+&emsp; Por último, usei a documentação 
+
+
--- app/src/main/java/carvalho/zanini/ponderada1/MainActivity.kt
+++ app/src/main/java/carvalho/zanini/ponderada1/MainActivity.kt
@@ -3,11 +3,13 @@ package carvalho.zanini.ponderada1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.random.Random
@@ -26,10 +28,11 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun LancadorDeDadosApp() {
+    var imagem by remember { mutableIntStateOf(0) }
     var dadoSelecionado by remember { mutableStateOf("D6") }
     var resultado by remember { mutableStateOf("Clique no botão para lançar o dado") }
 
-    val dados = listOf("D6",)
+    val dados = listOf("D6", "D10", "D20", "D100")
 
     Column(
         modifier = Modifier
@@ -42,6 +45,26 @@ fun LancadorDeDadosApp() {
             text = "Lançador de Dados",
             fontSize = 28.sp
         )
+
+        when (dadoSelecionado) {
+            "D6" -> {
+                val imagemDadoSelecionado = painterResource(id = R.drawable.d6)
+                Image(painter = imagemDadoSelecionado, contentDescription = null, modifier = Modifier.size(100.dp).align(Alignment.CenterHorizontally)
+                )
+            }
+            "D10" -> {
+                val imagemDadoSelecionado = painterResource(id = R.drawable.d10)
+                Image(painter = imagemDadoSelecionado, contentDescription = null, modifier = Modifier.size(100.dp).align(Alignment.CenterHorizontally))
+            }
+            "D20" -> {
+                val imagemDadoSelecionado = painterResource(id = R.drawable.d20)
+                Image(painter = imagemDadoSelecionado, contentDescription = null, modifier = Modifier.size(100.dp).align(Alignment.CenterHorizontally))
+            }
+            "D100" -> {
+                val imagemDadoSelecionado = painterResource(id = R.drawable.d100)
+                Image(painter = imagemDadoSelecionado, contentDescription = null, modifier = Modifier.size(100.dp).align(Alignment.CenterHorizontally))
+            }
+        }
 
         Spacer(modifier = Modifier.height(24.dp))
 
@@ -64,11 +87,25 @@ fun LancadorDeDadosApp() {
         Button(
             onClick = {
                 val valorSorteado = when (dadoSelecionado) {
-                    "D6" -> Random.nextInt(6)
+                    "D6" -> Random.nextInt(6) + 1
+                    "D10" -> Random.nextInt(10) + 1
+                    "D20" -> Random.nextInt(20) + 1
+                    "D100" -> Random.nextInt(100) + 1
                     else -> 0
                 }
 
+                imagem = when (valorSorteado.toString()) {
+                    "1" -> R.drawable._1
+                    "2" -> R.drawable._2
+                    "3" -> R.drawable._3
+                    "4" -> R.drawable._4
+                    "5" -> R.drawable._5
+                    "6" -> R.drawable._6
+                    else -> R.drawable._1
+                }
+
                 resultado = "Resultado do $dadoSelecionado: $valorSorteado"
+
             },
             modifier = Modifier.fillMaxWidth()
         ) {
@@ -76,6 +113,16 @@ fun LancadorDeDadosApp() {
         }
 
         Spacer(modifier = Modifier.height(24.dp))
+
+        if (dadoSelecionado == "D6" && imagem != 0) {
+            Image(
+                painter = painterResource(id = imagem),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp).align(Alignment.CenterHorizontally)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         Text(
             text = resultado,
